2d68f8a3f4da1907bb9062275cc8234f54710810
Alberto Luebbert M. <alberto.luebbert@medtrainer.com>
2024-11-20 15:19:08 -0600

    :sparkles: Render Props

--- src/components/RenderProps2/ChildComponent.tsx
+++ src/components/RenderProps2/ChildComponent.tsx
@@ -1,7 +1,18 @@
-const ChildComponent = () => {
+import { FC } from "react";
+
+interface ChildComponentProps {
+  data: string[];
+}
+
+const ChildComponent: FC<ChildComponentProps> = ({ data }) => {
   return (
-    <>
-    </>
+
+    data.map((item, index) => (
+      <li key={index}>
+        {item}
+      </li>
+    ))
+
   );
 };
 
--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,13 @@
-const ParentComponent = () => {
+import { ReactNode, FC, useState } from "react";
+
+interface ParentComponentProps {
+    render: (data: string[]) => ReactNode;
+}
+
+const ParentComponent: FC<ParentComponentProps> = ({ render }) => {
+    const [data] = useState<string[]>(["Apple", "Banana", "Orange"]);
+
+    return <ul>{render(data)}</ul>
 };
 
 export default ParentComponent;
