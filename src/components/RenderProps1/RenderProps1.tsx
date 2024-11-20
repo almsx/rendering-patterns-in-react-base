fe7038a36ce0d8dcde4b9928b8d1fde5094baafd
Alberto Luebbert <>
2024-11-19 20:47:59 -0600

    :sparkles: Config RenderProps

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,22 @@
+import { FC } from "react";
+
+type ChildComponentProps = {
+  render: (name: string) => JSX.Element;
+}
+
+export const ChildComponent: FC<ChildComponentProps> = ({ render }) => {
+  const name = "Almsx";
+  return (
+    <div>
+      {render(name)}
+    </div>
+  );
+};
+
+
 export const ParentComponent = () => {
   return (
-    <>
-    </>
+    <ChildComponent render={(name) => <p>Hello, {name}!</p>}>
+    </ChildComponent>
   );
 };
